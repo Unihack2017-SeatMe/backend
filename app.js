545906dcfbf96c159d3b6eb7325f9cb771042cc1
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-08-05 14:51:50 +1000

    Removed boilerplate

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-const port = process.env.PORT || 3456;
+const port = process.env.PORT || 8080;
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
-app.use('/', index);
-app.use('/users', users);
+app.get('/', function(req, res) {
+  res.json({ hello: 'world' });
+});
 
-app.post('/room/fullness/', function(req, res) {
+app.post('/room', function(req, res) {
 	var data = req.body;
-	console.log(data);
-	return res.json({'you sent': data})
+  console.log(data);
+  res.status(204);
+  res.send();
 });
 
 app.listen(port, () => {
